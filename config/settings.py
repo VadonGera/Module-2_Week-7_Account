0b0761290ef7a2114f253e353ad5721501513e02
Vadim Gera <horsespb@yandex.ru>
2024-04-23 23:06:29 +0300

    настройки приложения (API ключи, пути к файлам и т.д.)

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+S_RUS = 'ёйцукенгшщзхъфывапролджэячсмитьбю-'
+S_RUS_UPPER = S_RUS.upper()
+PATH_USERS_DATA = 'data/users.json'
+PATH_ACCOUNTS_DATA = 'data/accounts.json'
+PATH_EXCHANGE_DATA = 'data/exchange_rates.json'
+PATH_TRANSACTION_DATA = 'data/transactions.json'
+PATH_LOG_DATA = 'data/app.log'
+FORMAT_DATE = '%Y-%m-%d, %H:%M:%S'
